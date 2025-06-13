151f1f13bc424c51e09bf7bc1510eaa10178479a
SHAIKNASEERHUSSAIN-4780 <naseerhussain4780@gmail.com>
2025-06-13 14:38:45 +0530

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/area-checkpoint.py
+++
@@ -1,8 +0,0 @@
-def area(l,b):
-    '''
-    Objective: To compute the area of rectangle.
-    Input parameters: l,b - numeric values.
-    Return value: area - numeric value
-    '''
-    area=l*b
-    return area
