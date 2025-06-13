8b26cc69c4cb472f39bc12316cc7f33d0ad0810c
NASEERHUSSAIN123 <naseerhussain4780@gmail.com>
2025-06-13 11:44:46 +0530

    commit

---
+++ .ipynb_checkpoints/area-checkpoint.py
@@ -0,0 +1,8 @@
+def area(l,b):
+    '''
+    Objective: To compute the area of rectangle.
+    Input parameters: l,b - numeric values.
+    Return value: area - numeric value
+    '''
+    area=l*b
+    return area
